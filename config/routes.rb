1fd856c53c8e043e4e43c3bcee86f7d8d3d6b896
dangfp <dangfeipeng@gmail.com>
2015-01-14 16:47:49 +0800

    add add_category function and show category page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Myflix::Application.routes.draw do
   get '/home', to: "videos#index", as: 'home'
   get '/videos/:id', to: "videos#show", as: 'video_show'
 
+  resources :categories, only: [:new, :create, :show]
+
   get 'ui(/:action)', controller: 'ui'
 end
