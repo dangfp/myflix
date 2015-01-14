3f8aee3bd823a2c96de294b51644eea5ef4ba8a5
dangfp <dangfeipeng@gmail.com>
2015-01-14 11:22:38 +0800

    add home page and video show page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Myflix::Application.routes.draw do
+  get '/home', to: "videos#index", as: 'home'
+  get '/videos/:id', to: "videos#show", as: 'video_show'
+
   get 'ui(/:action)', controller: 'ui'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+videos = [
+  { title: 'first video', description: 'this is the first video that i make it myself', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/monk_large.jpg' },
+  { title: 'second video', description: 'this is the second video that i make it myself', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/monk_large.jpg' },
+  { title: 'third video', description: 'this is the third video that i make it myself', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg' },
+  { title: 'fourth video', description: 'this is the fourth video that i make it myself', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg' }
+]
 
-
-Video.create(title: "CSI",
- small_cover_url: "/public/tmp/monk.jpg",
- large_cover_url: "/public/tmp/monk_large.jpg",
- description: "由Gil Grissom领导的拉斯维加斯警局犯罪鉴证科夜班组的任务是深入案发现场寻找种种蛛丝马迹，并利用最先进的科技手段，把表面上没有任何联系、没有意义的线索拼凑成不可辩驳的证据，抽丝剥茧重组案情，让犯罪分子无处可逃……")
+videos.each do |attributes|
+  Video.find_or_initialize_by(title: attributes[:title]).tap do |video|
+    video.description = attributes[:description]
+    video.small_cover_url = attributes[:small_cover_url]
+    video.large_cover_url = attributes[:large_cover_url]
+    video.save!
+  end
+end
