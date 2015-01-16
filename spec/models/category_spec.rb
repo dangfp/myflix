ca26941e52b4da36f018dda7d877815eb921dfe7
dangfp <dangfeipeng@gmail.com>
2015-01-16 21:07:26 +0800

    Refactor test about category model and video model with shoulda-matcher.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,6 @@
 require 'spec_helper'
 
 describe Category do
-  it "is invalid without the name" do
-    expect(Category.new(name: nil).valid?).to be_falsey
-  end
-
-  it "saves itself" do
-    category = Category.new(name: 'Cartoon')
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos" do
-    cartoon = Category.create(name: 'Cartoon')
-    monk = Video.create(title: 'monk', description: 'pretty video.', category: cartoon)
-    family_guy = Video.create(title: 'family_guy', description: 'Funny video.', category: cartoon)
-
-    expect(cartoon.videos).to eq([family_guy, monk])
-  end
+  it { should validate_presence_of(:name) }
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,25 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "is invalid without title" do
-    expect(Video.new(title: nil).errors_on(:title).size).to eq(1)
-  end
-
-  it "is invalid without description" do
-    expect(Video.new(description: nil).errors_on(:title).size).to eq(1)
-  end
-
-  it "save itself" do
-    video = Video.new(title: 'First video', description: 'I liked a video.')
-    video.save
-
-    expect(Video.first).to eq(video)
-  end
-
-  it "belongs to category" do
-    cartoon = Category.create(name: 'Cartoon')
-    monk = Video.create(title: 'monk', description: 'Funny video.', category: cartoon)
-
-    expect(monk.category).to eq(cartoon)
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should belong_to(:category) }
 end
