fd8dbf03638cc1a4e42e8bb4599b358276523960
dangfp <dangfeipeng@gmail.com>
2015-01-16 19:50:29 +0800

    Test the category model and associations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,7 +4,14 @@ describe Video do
   it "save itself" do
     video = Video.new(title: 'First video', description: 'I liked a video.')
     video.save
-    
+
     expect(Video.first).to eq(video)
+  end
+
+  it "belongs to category" do
+    cartoon = Category.create(name: 'Cartoon')
+    monk = Video.create(title: 'monk', description: 'Funny video.', category: cartoon)
+
+    expect(monk.category).to eq(cartoon)
   end
 end
