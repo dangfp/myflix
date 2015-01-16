8989a59fc62344575eda6e9a52ad9d990c19acec
dangfp <dangfeipeng@gmail.com>
2015-01-16 20:03:11 +0800

    ned

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Video do
+  it "is invalid without title"
+
+  it "is invalid without description"
+
   it "save itself" do
     video = Video.new(title: 'First video', description: 'I liked a video.')
     video.save
