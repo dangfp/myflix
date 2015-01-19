d341781e023d104ac1e7d0f7323a0bfa79e8a495
dangfp <dangfeipeng@gmail.com>
2015-01-19 18:05:16 +0800

    Implement sign_in and sign_out.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You've signed in."
+      redirect_to home_path
+    else
+      flash[:error] = "There's something wrong with your email or password."
+      redirect_to sign_in_path
+    end
+  end
+
+  def destory
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
